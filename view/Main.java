1bcd23c462968d2518ecc43cca3c53058182ef05
Mick <mickzeller@gmail.com>
2016-12-26 19:06:16 -0700

    turn history will be stored within a .txt file for clients to all be on the correct board.  Testing the file output that will be stored on the server.

--- view/Main.java
+++ view/Main.java
@@ -1,7 +1,10 @@
 package view;
 
+import control.GameHub;
 import model.Board;
-import model.Tile;
+
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * Name: Mick Zeller
@@ -13,5 +16,14 @@ public class Main
     public static void main(String[] args)
     {
 
+        try (FileWriter printWriter = new FileWriter("board.txt"))
+        {
+            Board myBoard = new Board();
+            GameHub.recordingFile(myBoard, printWriter);
+
+        } catch (IOException exception)
+        {
+            System.out.println("Cannot process file: " + exception);
+        }
     }
 }
