893207e9f44f51a30c500dd10aa1021b98a0f9a7
Mick <mickzeller@gmail.com>
2016-12-31 22:41:56 -0700

    Created nested class within BoardPanel to handle events

--- view/BoardPanel.java
+++ view/BoardPanel.java
@@ -2,7 +2,10 @@ package view;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 
 /**
@@ -37,7 +40,7 @@ public class BoardPanel extends JPanel
             tiles[ i ] = button;
             tiles[ i ].setIcon(icons[ 0 ]);
             tiles[ i ].setActionCommand(String.valueOf(i));
-
+            tiles[ i ].addActionListener(new TileHandler());
 
             add(tiles[ i ]);
             tileNumber++;
@@ -53,4 +56,92 @@ public class BoardPanel extends JPanel
             size--;
         }
     }
+
+
+    private class TileHandler implements ActionListener
+    {
+
+        //private JButton updatingTile;
+
+        @Override
+        public void actionPerformed(ActionEvent e)
+        {
+
+
+            try
+            {
+                int tileLocation = Integer.parseInt(e.getActionCommand());
+                System.out.println("Tile location " + tileLocation);
+
+
+            } catch (Exception ex)
+            {
+                System.out.println(ex.toString());
+            }
+
+            // TODO: 12/31/2016 implement a way to send this data to the server 
+
+        }
+
+
+//        public void updatingBoard(String userName)
+//        {
+//            int t = 0;
+//            String temp = gs.currentBoard.getJson().toString();
+//            System.out.println(gs.currentBoard.getJson());
+//            for (int i = 0; i < gs.currentBoard.getJson().length(); i++)
+//            {
+//                t = Integer.parseInt(temp.substring(i, i + 1));
+//                tileStatus[ i ] = t;
+//                testTileUpdate(i, tileStatus[ i ]);
+//            }
+//
+//        }
+
+        public void testTileUpdate(int index, int status)
+        {
+
+            if (status < 0 || status > 9 || index > tiles.length - 1)
+            {
+                return;
+            }
+            switch (status)
+            {
+                case 0:
+                    button = tiles[ index ];
+                    tiles[ index ] = button;
+                    break;
+                case 1:
+                    button = tiles[ index ];
+                    button.setBorder(new LineBorder(Color.RED, 3));
+                    tiles[ index ] = button;
+                    break;
+                case 2:
+                    button = tiles[ index ];
+                    button.setBorder(new LineBorder(Color.BLUE, 3));
+                    tiles[ index ] = button;
+                    break;
+                case 3:
+                    button = tiles[ index ];
+                    button.setBorder(new LineBorder(Color.YELLOW, 3));
+                    tiles[ index ] = button;
+                    break;
+                case 4:
+                    button = tiles[ index ];
+                    button.setEnabled(false);
+                    button.setBorder(new LineBorder(Color.GREEN, 3));
+                    tiles[ index ] = button;
+                    break;
+
+                default:
+                    button = tiles[ index ];
+                    button.setBorder(new LineBorder(Color.PINK, 3));
+                    tiles[ index ] = button;
+                    break;
+            }
+        }
+
+
+    }
+
 }
